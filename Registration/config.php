2b46dc727c326e52cad4110822699c736a9956ab
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-17 00:52:09 +0200

    connect the database headings to the config file

---
+++ Registration/config.php
@@ -0,0 +1,11 @@
+<?php 
+#add databases cresenditals of new customers 
+define('DB_NAME' , 'Registration');
+define('DB_USER', 'Admin_user');
+define('DB_PASSWORD', 'QWE$RTY&UIO)');
+define('DB_HOST', 'localhost');
+
+#connect the database values to mysqli_connect
+
+
+?>
