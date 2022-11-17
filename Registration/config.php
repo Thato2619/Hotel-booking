d06ff65140c5b17c54d8862a61c1ebec65882d18
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-17 02:06:44 +0200

    add conditional statement if connection fails to save

--- Registration/config.php
+++ Registration/config.php
@@ -6,6 +6,10 @@ define('DB_PASSWORD', 'QWE$RTY&UIO)');
 define('DB_HOST', 'localhost');
 
 #connect the database values to mysqli_connect
-
+$connection = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME );
+#add conditional to see if the database is connected 
+if($connection === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 
 ?>
