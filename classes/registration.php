ceae6179443b172a80de25ffb65cd45dc9f4e9e2
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-19 00:05:32 +0200

    create registration class in registration.php file

---
+++ classes/registration.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Registration{
+    private $registration_id;
+    private $name;
+    private $surname;
+    private $age;
+    private $phone;
+    private $email;
+    private $password;
+}
+
+
+?>
