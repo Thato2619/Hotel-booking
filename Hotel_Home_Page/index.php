d3643f4693b32e9e3b9e55e5a854043bf14fac4b
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-18 03:56:31 +0200

    add 6 cards with image and description from bootstrap

--- Hotel_Home_Page/index.php
+++ Hotel_Home_Page/index.php
@@ -70,8 +70,10 @@
     <div class="row">
         <div class="col-md-10">
             <!-- Types of room in the hotel -->
-            <div class="row">
-                <div class="col-md-4">
+
+            
+            <div class="row " >
+                <div class="col-md-4 mb-2">
                     <div class="card">
                         <img src="..." class="card-img-top" alt="...">
                         <div class="card-body">
@@ -81,7 +83,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4  mb-4">
                     <div class="card">
                         <img src="..." class="card-img-top" alt="...">
                         <div class="card-body">
@@ -91,30 +93,62 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 mb-4">
                     <div class="card">
                         <img src="..." class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card">
-                                <img src="..." class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                                <div class="col-md-4"></div>
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
+    </div>
+
+    </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-10">
+            <!-- Types of room in the hotel -->
+            <div class="row">
+                <div class="col-md-4  mb-4">
+                    <div class="card">
+                        <img src=".." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4  mb-4">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4  mb-4">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
     </div>
 
 
