ead7d604c8c62b82b21f8d1f15922e7c7698849b
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-18 04:54:38 +0200

    add two more buttons and new style.css file

--- Hotel_Home_Page/index.php
+++ Hotel_Home_Page/index.php
@@ -79,7 +79,8 @@
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <a href="#" class="btn btn-success">Reserve</a>
+                            <a href="#" class="btn btn-warning">View More</a>
                         </div>
                     </div>
                 </div>
@@ -90,7 +91,8 @@
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <a href="#" class="btn btn-success">Reserve</a>
+                            <a href="#" class="btn btn-warning">View More</a>
                         </div>
                     </div>
                 </div>
@@ -101,7 +103,8 @@
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <a href="#" class="btn btn-success">Reserve</a>
+                            <a href="#" class="btn btn-warning">View More</a>
                         </div>
                     </div>
                 </div>
@@ -123,7 +126,8 @@
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <a href="#" class="btn btn-success">Reserve</a>
+                            <a href="#" class="btn btn-warning">View More</a>
                         </div>
                     </div>
                 </div>
@@ -135,7 +139,8 @@
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <a href="#" class="btn btn-success">Reserve</a>
+                            <a href="#" class="btn btn-warning">View More</a>
                         </div>
                     </div>
                 </div>
@@ -147,7 +152,8 @@
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <a href="#" class="btn btn-success">Reserve</a>
+                            <a href="#" class="btn btn-warning">View More</a>
                         </div>
                     </div>
                 </div>
