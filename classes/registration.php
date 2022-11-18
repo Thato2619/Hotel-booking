7ca717f9aebf79f7674a45ac11a42280493884f1
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-19 00:12:21 +0200

    place all the registration values in  constructor()

--- classes/registration.php
+++ classes/registration.php
@@ -8,7 +8,18 @@ class Registration{
     private $phone;
     private $email;
     private $password;
+
+    public function __construct($registration_id, $name, $surname, $age, $phone, $email, $password) {
+        $this->registration_id = $registration_id;
+        $this->name = $name;
+        $this->surname = $surname;
+        $this->age = $age;
+        $this->phone = $phone;
+        $this->email= $email;
+        $this->password = $password;
+    }
 }
+
 
 
 ?>
