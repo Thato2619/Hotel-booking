674259c08e4803df12707c4b055b60d5874280d4
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-19 00:42:48 +0200

    Create a method named emptyInput()

--- classes/registration.php
+++ classes/registration.php
@@ -1,7 +1,6 @@
 <?php 
 
 class Registration{
-    private $registration_id;
     private $name;
     private $surname;
     private $age;
@@ -9,8 +8,7 @@ class Registration{
     private $email;
     private $password;
 
-    public function __construct($registration_id, $name, $surname, $age, $phone, $email, $password) {
-        $this->registration_id = $registration_id;
+    public function __construct($name, $surname, $age, $phone, $email, $password) {
         $this->name = $name;
         $this->surname = $surname;
         $this->age = $age;
@@ -18,9 +16,16 @@ class Registration{
         $this->email= $email;
         $this->password = $password;
     }
-
+    #create a method for when registration input is left empty
     private function emptyInput() {
-        
+        $results;
+        if(empty($this->$name) || empty($this->$surname) || empty($this->$age) || empty($this->$phone || empty($this->$password) || )) {
+            $results = false;
+        }
+        else{
+            $results = true;
+        }
+        return $results;
     }
 }
 
