f59024fb3cce51dce0c768d55783100cd2134a89
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-18 10:33:30 +0200

    add  html boilerplate in index.php with registration page

---
+++ Registration/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Registration Page</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+        <h1>Registration</h1>
+        <!-- add error message in php tags -->
+        <?php 
+            if(isset($_GET['error'])) {?>
+            <p class="error"> <?php echo $_GET['error']; ?></p>
+            <?php } ?>
+            <label>Email: </label>
+            <input type="email" name="email" placeholder="Email address" required> <br>
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Password" required> <br>
+
+            <button type="submit">LOGIN</button>
+    </form>
+</body>
+</html>
