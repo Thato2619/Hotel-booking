23feaa20c6ceaf087424ff0e4c7e2d1c7bfc0580
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-18 04:04:44 +0200

    add image of deluxe_bedroom in 2nd card

--- Hotel_Home_Page/index.php
+++ Hotel_Home_Page/index.php
@@ -83,9 +83,10 @@
                         </div>
                     </div>
                 </div>
+                <!-- card of deluxe bedroom -->
                 <div class="col-md-4  mb-4">
                     <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
+                        <img src="./Images/deluxe_room.jpeg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
