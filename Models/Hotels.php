b9d87c2c15f42cf5bee061d2e9574edeff8df045
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-20 00:52:25 +0200

    add constructor for adjacent functions in hotel fields

--- Models/Hotels.php
+++ Models/Hotels.php
@@ -4,10 +4,22 @@ class Hotels {
     // fields of hotels 
     private $id;
     private $name;
-    private $PricePerPrice;
+    private $pricePerPrice;
     private $availableRooms;
-    private $totalBooks;
+    private $totalBookings;
     private $rating;
+
+    //constructor
+    public function __construct($id, $name, $pricePerPrice, $availableRooms, $totalBookings, $rating)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->pricePerPrice = $pricePerPrice;
+        $this->availableRooms = $availableRooms;
+        $this->totalBookings = $totalBookings;
+        $this->rating = $rating;
+
+    }
 }
 
 
