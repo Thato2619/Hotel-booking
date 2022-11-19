efb40ef8dabeb89fecac145f8f4624d976b48cc7
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-20 01:04:56 +0200

    work on constructor

--- Models/Hotels.php
+++ Models/Hotels.php
@@ -4,21 +4,23 @@ class Hotels {
     // fields of hotels 
     private $id;
     private $name;
-    private $pricePerPrice;
+    private $price;
     private $availableRooms;
     private $totalBookings;
     private $rating;
+    private $stayDays;
 
     //constructor
-    public function __construct($id, $name, $pricePerPrice, $availableRooms, $totalBookings, $rating)
+    public function __construct($id, $name, $price, $availableRooms, $totalBookings, $rating, $stayDays)
     {
         $this->id = $id;
         $this->name = $name;
-        $this->pricePerPrice = $pricePerPrice;
+        $this->price = $price;
         $this->availableRooms = $availableRooms;
         $this->totalBookings = $totalBookings;
         $this->rating = $rating;
-
+        $this->stayDays = $stayDays;
+        
     }
 }
 
