5a8523a38371922b7ab787d2ed9c15c289c1e7e5
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-20 00:39:59 +0200

    create Hotels class with field aligned to hotel

---
+++ Models/Hotels.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Hotels {
+    // fields of hotels 
+    private $id;
+    private $name;
+    private $PricePerPrice;
+    private $availableRooms;
+    private $totalBooks;
+    private $rating;
+}
+
+
+
+
+?>
