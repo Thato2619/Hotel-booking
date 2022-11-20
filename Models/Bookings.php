c6601d0df97b0ace4886284eac98d3902f525bcc
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-20 04:52:46 +0200

    add property fields in bookings class file

---
+++ Models/Bookings.php
@@ -0,0 +1,19 @@
+<?php 
+class Bookings{
+    private $id;
+    private $hotelName;
+    private $customerEmail;
+    private $phoneNumber;
+    private $hotelId;
+    private $customerId;
+    private $checkInDate;
+    private $checkOutDate;
+    private $totalCost;
+
+}
+
+
+
+
+
+?>
--- Models/New_Customer.php
+++ Models/New_Customer.php
@@ -10,7 +10,7 @@ class New_Customer{
 
         //SQL statement to create or insert new user
         $statement = "INSERT INTO Users (id, fullName, Age, phone, password, email, staff_id, role)" .
-        "VALUES ('".$Users->getId()."','".$Users->getUsername()."','".$Users->getPassword()."','".$Users->getEmail()."','".$Users->getRole()."')";
+        "VALUES ('".$Users->getId()."','".$Users->getFullName()."','".$Users->getPassword()."','".$Users->getEmail()."','".$Users->getRole()."')";
 
         //send in request
         if($result = $connection ->query($statement)) {
@@ -23,12 +23,48 @@ class New_Customer{
         }
 
         //create function to read new user by ID
-        public function getByID($DBConfig, $id) {
+        public function getByID($DBConfig, $getId) {
+            //connect via database
+            $connection = $DbConfig->connectToDatabase();
+
+            //SQL statement
+            $statement = "SELECT * FROM Users WHERE Customer_id='$getId";
+            
+            if ($result = $connection->query($statement)) {
+
+            // get 1 result
+            $row = $result->fetch_object(); // returns a PHP stdObject
+
+            
+            if ($row !== null) {
+            
+                $userObject = new User(
+                    $row->fullName,
+                    $row->password,
+                    $row->email,
+                    $row->role
+                );
+                    
+                $userObject->setId($row->id);
+
+                return $userObject;
+            } 
+            } 
+                                        
+    
+            else {
+
+            echo $connection->error;
+            die("Connection failed: " . $connection->connect_error); //die function to close connection in case of error
         }
+
+        $connection->close();
     }
+    
+
 }
 
-
+}
 
 
 
