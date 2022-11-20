2b4968aabf0fdbf830c4fc317bd2b3f994697cb9
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-20 02:42:32 +0200

    create properties / fields in users class

--- Models/User.php
+++ Models/User.php
@@ -1,6 +1,17 @@
 <?php 
 
+class Users{
+    //create private fields in Users class
+    private $id;
+    private $fullName;
+    private $age;
+    private $phone;
+    private $email;
+    private $password;
+    private $role;
 
+    
+}
 
 
 
